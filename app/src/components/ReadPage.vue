d8a91610527963dbbf98232d9a51d083ab7d9028
Anay Dandekar <anaydbcp@gmail.com>
2026-05-31 21:50:37 -0400

    Fixed header styling

--- app/src/components/ReadPage.vue
+++ app/src/components/ReadPage.vue
@@ -166,27 +166,67 @@ function cancelGrab() {
 
 <template>
     <!-- main page content -->
-    <div class="d-flex flex-column vh-100 p-3">
-        <div class="row text-center">
-            <div class="d-flex justify-content-center align-items-center">
-                <div class="w-100 d-flex align-items-center justify-content-center position-relative">
-                    <p class="fs-1 fw-bold m-0 text-center w-100">Fast Lit</p>
-                    <i
-                        class="ms-3 bi bi-gear fs-4 align-middle btn position-absolute"
-                        style="right: 0; vertical-align: middle;"
-                        role="img"
-                        data-bs-toggle="modal"
-                        data-bs-target="#settingsModal"
-                    ></i>
-                </div>
-       
-            </div>
-   
+    <div class="bg-deepblue min-h-screen p-5">
+        <!-- page header -->
+        <div class="">
+            <p class="text-white text-center text-4xl font-bold">Fast Lit</p>
+            <!-- <i
+                class="ms-3 bi bi-gear fs-4 align-middle btn position-absolute"
+                style="right: 0; vertical-align: middle;"
+                role="img"
+                data-bs-toggle="modal"
+                data-bs-target="#settingsModal"
+            ></i> -->
+            <button
+                class="btn btn-circle btn-ghost absolute top-5 right-5"
+                style="background: transparent; box-shadow: none; border: none;"
+            >      
+                <!-- svg for a gear icon for the settings button -->
+                <svg
+                    version="1.1"
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    x="0px"
+                    y="0px"
+                    width="32px"
+                    height="32px"
+                    viewBox="0 0 122.88 122.878"
+                    fill="white"
+                >
+                    <g>
+                        <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="
+                                M101.589,14.7l8.818,8.819c2.321,2.321,2.321,6.118,0,8.439l-7.101,7.101
+                                c1.959,3.658,3.454,7.601,4.405,11.752h9.199c3.283,0,5.969,2.686,5.969,5.968V69.25
+                                c0,3.283-2.686,5.969-5.969,5.969h-10.039c-1.231,4.063-2.992,7.896-5.204,11.418l6.512,6.51
+                                c2.321,2.323,2.321,6.12,0,8.44l-8.818,8.819c-2.321,2.32-6.119,2.32-8.439,0l-7.102-7.102
+                                c-3.657,1.96-7.601,3.456-11.753,4.406v9.199c0,3.282-2.685,5.968-5.968,5.968H53.629
+                                c-3.283,0-5.969-2.686-5.969-5.968v-10.039c-4.063-1.232-7.896-2.993-11.417-5.205l-6.511,6.512
+                                c-2.323,2.321-6.12,2.321-8.441,0l-8.818-8.818c-2.321-2.321-2.321-6.118,0-8.439l7.102-7.102
+                                c-1.96-3.657-3.456-7.6-4.405-11.751H5.968C2.686,72.067,0,69.382,0,66.099V53.628
+                                c0-3.283,2.686-5.968,5.968-5.968h10.039c1.232-4.063,2.993-7.896,5.204-11.418l-6.511-6.51
+                                c-2.321-2.322-2.321-6.12,0-8.44l8.819-8.819c2.321-2.321,6.118-2.321,8.439,0l7.101,7.101
+                                c3.658-1.96,7.601-3.456,11.753-4.406V5.969C50.812,2.686,53.498,0,56.78,0h12.471
+                                c3.282,0,5.968,2.686,5.968,5.969v10.036c4.064,1.231,7.898,2.992,11.422,5.204l6.507-6.509
+                                C95.471,12.379,99.268,12.379,101.589,14.7L101.589,14.7z
+                                M61.44,36.92c13.54,0,24.519,10.98,24.519,24.519c0,13.538-10.979,24.519-24.519,24.519
+                                c-13.539,0-24.519-10.98-24.519-24.519C36.921,47.9,47.901,36.92,61.44,36.92L61.44,36.92z
+                            "
+                        />
+                  
+                    </g>
+                </svg>
+            </button>
+
+
         </div>
-        <div class="row flex-grow-1">
-            <div class="col p-1">
-                <Reader :text="text" :wpm="Number(wpm)" />
-            </div>
+
+        <!-- page body -->
+        <div class="">
+            <Reader :text="text" :wpm="Number(wpm)" />
         </div>
     </div>
 
