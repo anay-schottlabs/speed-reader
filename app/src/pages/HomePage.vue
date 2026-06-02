8b0b190ce0dad99a99774c17b4fb6cae3fbc7910
Anay Dandekar <anaydbcp@gmail.com>
2026-06-02 19:39:07 -0400

    Added template for hero component for homepage

--- app/src/pages/HomePage.vue
+++ app/src/pages/HomePage.vue
@@ -1,10 +1,53 @@
 <script setup>
 import Header from '../components/Header.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function navigateTo(path) {
+    router.push(path);
+}
 </script>
 
 <template>
     <div>
         <Header pageName="" />
+        <div class="hero min-h-screen">
+            <div class="hero-content text-center">
+                <div class="max-w-3xl mx-auto px-4">
+                    <h1 class="text-7xl font-bold text-red whitespace-normal">Unlock your potential.</h1>
+                    <p class="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button
+                        class="btn btn-wide bg-red hover:opacity-80 relative"
+                        @click="navigateTo('/read')"
+                    >
+                        <span class="w-full text-center">
+                            Start Reading
+                        </span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="white"
+                            viewBox="0 0 24 24"
+                            width="28"
+                            height="28"
+                            class="absolute right-4 top-1/2 -translate-y-1/2"
+                        >
+                            <path
+                                d="M5 12h14M13 6l6 6-6 6"
+                                stroke="white"
+                                stroke-width="2.2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                fill="none"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
